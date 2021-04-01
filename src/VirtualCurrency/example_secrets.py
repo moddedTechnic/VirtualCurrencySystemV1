d881934dcbc900676630a5f7c1d41f87ef02c99b
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-01 13:29:28 +0100

    Added docstring to example secrets

--- src/VirtualCurrency/example_secrets.py
+++ src/VirtualCurrency/example_secrets.py
@@ -1,3 +1,6 @@
+'''
+An example secrets file containing the secrets which shouldn't be made public
+'''
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'secret-key'
