6aff383848de1880939542fe5ffbad06c49e606e
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-01 13:21:32 +0100

    Moved secrets to _secrets

--- src/VirtualCurrency/settings.py
+++ src/VirtualCurrency/settings.py
@@ -12,15 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from ._secrets import * #pylint: disable=unused-wildcard-import disable=wildcard-import
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9t9odclp1*&_34s##$&g5x@xz!=erzv6%+j(v**dznbj)3m!=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
