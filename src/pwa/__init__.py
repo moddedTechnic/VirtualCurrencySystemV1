549d8c36b2d8d057d73993be667b227111fd5130
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-03 20:23:44 +0100

    Added default settings for PWA

--- src/pwa/__init__.py
+++ src/pwa/__init__.py
@@ -5,3 +5,19 @@ Includes:
 - manifest.json (loaded from static files)
 - robots.txt (loaded from static files)
 '''
+
+from django.conf import settings as user_settings
+from . import default_settings
+
+
+class AppSettings:
+    def __getattr__(self, name):
+        if hasattr(user_settings, name):
+            return getattr(user_settings, name)
+        if hasattr(default_settings, name):
+            return getattr(default_settings, name)
+        return getattr(user_settings, name)
+
+
+settings = AppSettings()
+__all__ = ['settings']
---
+++ src/pwa/default_settings.py
@@ -0,0 +1,11 @@
+'''
+Default values for the custom PWA settings
+'''
+
+LOAD_SERVICE_WORKER = False
+
+MANIFEST = {}
+MANIFEST_FILE = False
+
+ROBOTS = {}
+ROBOTS_FILE = False
