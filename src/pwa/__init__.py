c7a1b7e300a96938b3dceab6f2f2696ae4a77951
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-03 20:39:29 +0100

    Added check if item is dict to pass defaults in

--- src/pwa/__init__.py
+++ src/pwa/__init__.py
@@ -11,9 +11,15 @@ from . import default_settings
 
 
 class AppSettings:
+    'Settings for the app - manages defaults'
     def __getattr__(self, name):
         if hasattr(user_settings, name):
-            return getattr(user_settings, name)
+            item = getattr(user_settings, name)
+            if isinstance(item):
+                base_item: dict = getattr(default_settings, name)
+                base_item.update(item)
+                return base_item
+            return item
         if hasattr(default_settings, name):
             return getattr(default_settings, name)
         return getattr(user_settings, name)
