0fb8bc32219c9f6fd26dfa370ec38e886fcc0d3b
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-03 15:14:39 +0100

    Moved file serving o utils

--- src/pwa/urls.py
+++ src/pwa/urls.py
@@ -3,14 +3,13 @@ Urls for the PWA app
 '''
 
 from django.urls import path
-
-from .views import serve_static_file
+from utils import serve
 
 app_name = 'pwa'
 
 urlpatterns = [
-    path('manifest.json', serve_static_file('manifest.json')),
-    path('manifest', serve_static_file('manifest.json')),
-    path('robots.txt', serve_static_file('robots.txt')),
-    path('robots', serve_static_file('robots.txt')),
+    path('manifest.json', serve.static_file('manifest.json')),
+    path('manifest', serve.static_file('manifest.json')),
+    path('robots.txt', serve.static_file('robots.txt')),
+    path('robots', serve.static_file('robots.txt')),
 ]
---
+++ src/utils/serve.py
@@ -0,0 +1,52 @@
+'''
+Utilities related to serving files
+'''
+
+from pathlib import Path
+from typing import Callable, Optional, Union
+
+from django.conf import settings
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import Http404, HttpResponse
+
+class _Result:
+    'Stores a result, either ok or error'
+    def __init__(self, *, ok=None, err=None):
+        self.ok = ok
+        self.err = err
+
+    def __bool__(self):
+        return self.err is None
+
+    def get(self):
+        if self:
+            return self.ok
+        raise self.err
+
+def _load_static_file(
+    filename: Union[str, Path],
+    content_type: Optional[str] = None):
+    for directory in settings.STATICFILES_DIRS:
+        fpath: Path = directory / filename
+        if fpath.exists():
+            content_type = (
+                content_type or {
+                    'json': 'application/json',
+                    'txt': 'text/plain',
+                }.get(fpath.suffix[1:], 'text/plain')
+            )
+            with open(fpath, 'r') as f:
+                data = f.read()
+            return _Result(ok=HttpResponse(data, content_type=content_type))
+    return _Result(err=Http404(f'Could not find {filename}'))
+
+def static_file(
+        filename: Union[str, Path],
+        content_type: Optional[str] = None
+    ) -> Callable[[WSGIRequest], HttpResponse]:
+    result = _load_static_file(filename, content_type)
+
+    return lambda _: result.get()
+
+
+__all__ = [ 'static_file' ]
