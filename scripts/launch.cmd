0c23ac5f5440ed81cc46b6d5606644da706908a8
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-04 14:29:28 +0100

    Corrected launch script

--- scripts/launch.cmd
+++ scripts/launch.cmd
@@ -4,9 +4,9 @@ cls
 IF "%1" == "" (
     SET host="80"
 ) ELSE (
-    SET host="%1"
+    SET host="%~1"
 )
 
-IF "%2" NEQ "" SET host="%host%:%2"
+IF "%2" NEQ "" SET host="%host%:%~2"
 
-python src\manage.py runserver "%host%"
+python src\manage.py runserver %host%
