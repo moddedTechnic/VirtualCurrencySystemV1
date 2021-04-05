2117430d9edea7c3b8c62338a914821e4aec2001
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-05 11:28:20 +0100

    Stop trying to unwrap Path

--- src/utils/load.py
+++ src/utils/load.py
@@ -16,9 +16,11 @@ def static_file(
         filename: Union[str, Path],
         content_type: Optional[str] = None):
     try:
-        data, fpath = static.path(filename).unwrap()
+        fpath: Path = static.path(filename).unwrap()
     except Exception as e: #pylint: disable=broad-except
         return Result(err=e)
+    with fpath.open('r') as f:
+        data = f.read()
     content_type = static.mime_type(fpath)
     return Result(ok=(data, fpath, content_type))
 
