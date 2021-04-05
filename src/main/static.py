f8528d9823f5ef2e0bd2c177976a629013d5547f
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-05 11:27:52 +0100

    Corrected to get content type from load

--- src/main/static.py
+++ src/main/static.py
@@ -5,12 +5,11 @@ Serves static files with the correct MIME type
 from django.http.response import HttpResponse
 from django.urls import path
 
-from utils import load, static
+from utils import load
 
 def serve_static(request, resource):
     del request  # unused
-    data, fpath = load.static_file(resource).unwrap()
-    content_type = static.mime_type(fpath)
+    data, _, content_type = load.static_file(resource).unwrap()
     return HttpResponse(data, content_type=content_type)
 
 
