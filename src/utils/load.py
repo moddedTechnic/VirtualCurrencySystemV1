65f9c14abd4b73cbd91252d5eeb645215629c5e5
Jonathan Leeming <jpleeming51@gmail.com>
2021-04-05 11:15:35 +0100

    Added MIME type sniffer

--- src/utils/load.py
+++ src/utils/load.py
@@ -5,9 +5,8 @@ Utilities related to loading files
 from pathlib import Path
 from typing import Optional, Union
 
-from django.conf import settings
+from . import Result, static
 
-from . import Result
 
 class Error(Exception):
     'An error ocurred whilst loading a resource'
@@ -16,19 +15,11 @@ class Error(Exception):
 def static_file(
         filename: Union[str, Path],
         content_type: Optional[str] = None):
-    for directory in settings.STATICFILES_DIRS:
-        fpath: Path = directory / filename
-        if fpath.exists():
-            content_type = (
-                content_type or {
-                    'js': 'application/javascript',
-                    'json': 'application/json',
-                    'txt': 'text/plain',
-                }.get(fpath.suffix[1:], 'text/plain')
-            )
-            with open(fpath, 'r') as f:
-                data = f.read()
-            return Result(ok=(data, fpath))
-    return Result(err=f'Could not find {filename}')
+    try:
+        data, fpath = static.path(filename).unwrap()
+    except Exception as e: #pylint: disable=broad-except
+        return Result(err=e)
+    content_type = static.mime_type(fpath)
+    return Result(ok=(data, fpath, content_type))
 
 __all__ = ['static_file']
--- src/utils/static.py
+++ src/utils/static.py
@@ -14,6 +14,8 @@ from . import Result
 
 
 def path(filename: Union[str, Path]) -> Result:
+    if isinstance(filename, str):
+        filename = filename.strip('/')
     for directory in settings.STATICFILES_DIRS:
         fpath: Path = directory / filename
         if fpath.exists():
@@ -21,5 +23,16 @@ def path(filename: Union[str, Path]) -> Result:
     result = Result(err=f'File not found ({filename})')
     result.wrap_err(FileNotFoundError)
     return result
+
+def mime_type(filename: Union[str, Path]) -> str:
+    filename = Path(filename)
+    suffix = filename.suffix[1:]
+    return {
+        'js': 'application/javascript',
+        'json': 'application/json',
+        'css': 'text/css',
+        'html': 'text/html',
+        'txt': 'text/plain',
+    }.get(suffix, 'text/plain')
 
 __all__ = ['path']
